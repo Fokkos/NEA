fd5837ee5d900f1fee9c80877a648fe57bce7ea6
Fokkos <54935498+Fokkos@users.noreply.github.com>
2020-01-10 11:42:27 +0000

    update user info forms

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,7 @@
 from django import forms #Library used for form specific commands
 from django.contrib.auth.models import User #Imports the user information
 from django.contrib.auth.forms import UserCreationForm #Imports the form that I wish to inherit off of
-
+from .models import Profile
 class RegistrationForm(UserCreationForm): #The new registration form that includes email!
     email=forms.EmailField(required=True) #Specifies that a valid email address must be entered, and the field is not optional
 
@@ -10,4 +10,13 @@ class RegistrationForm(UserCreationForm): #The new registration form that includ
         fields=["username","email","password1","password2"] #defines the fields that will appear in the form and the order they appear in
                                                             #password1 is the initial password input and password2 is the check that theyre the same
 
-                                                            
+class updateUser(forms.ModelForm): #The new registration form that includes email!
+    email=forms.EmailField() #Specifies that a valid email address must be entered, and the field is not optional
+    class Meta:
+        model=User #Defines the model that will be affected by this change, which is the User model
+        fields=["username","email"] #defines the fields that will appear in the form and the order they appear in
+
+class updateProdile(forms.ModelForm):
+    class Meta:
+        model=Profile
+        fields=["description","pfp"]
--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User #Imports the user model
-from django.dispatch import receiver
+from django.dispatch import receiver #Imports the signal reciever, used to initiate functions upon a certaon signal
 from django.db.models.signals import post_save #Signal sent at the end of a save() in order to save the files
 # Create your models here.
 
@@ -9,11 +9,13 @@ class Profile(models.Model): #Defines the model that extends the User model
     pfp = models.ImageField(default='media/pfps/default.png', upload_to='media/pfps') #Define field that will hold profile pictures
     description = models.TextField(max_length=200, default="") #User Profile description
     
-    def __str__(self):
+    def __str__(self): #Defines how the titles of each profile is shown in the admin view
         return f'{self.user.username}\'s Profile'
 
 
-@receiver(post_save, sender=User)
-def ensure_profile_exists(sender, **kwargs):
-    if kwargs.get('created', False):
-        Profile.objects.get_or_create(user=kwargs.get('instance'))    
+@receiver(post_save, sender=User) #When User model is saved, the following function happens
+def create_profile(sender, **kwargs): #Defines function that creates a profile if one doesnt already exist
+    if kwargs.get('created', False): #If a Profile has not been created already, do the following
+        Profile.objects.get_or_create(user=kwargs.get('instance')) #Creates a profile linked to the current instance
+
+
