2b165b225b2d28a5061f39259514c11b075cffbf
Fokkos <54935498+Fokkos@users.noreply.github.com>
2020-01-12 21:17:20 +0000

    delete posts functionality

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,14 @@
 from . import views
 from django.urls import path
-from .views import viewPost, createPost
+from .views import viewPost, createPost, editPost, deletePost
 
 #urlpatterns links urls to certain views or functions
 urlpatterns = [
     path('', views.index, name='index'), #Index is the homepage and shows an index of all posts on the site
     path('search/', views.search, name='search'), #URL for the search functionality of the site
     path('<str:userID>/post/<slug:slug>/', viewPost.as_view(), name = 'post-detail'), #URL for individual posts
-    path('create/',createPost.as_view(), name='post-create')
+    path('create/',createPost.as_view(), name='post-create'), #URL for post creation
+    path('<str:userID>/post/<slug:slug>/edit/', editPost.as_view(), name = 'post-update'), #URL for updating posts
+    path('<str:userID>/post/<slug:slug>/delete/', deletePost.as_view(), name = 'post-delete'), #URL for updating posts
 ]
 
--- main/views.py
+++ main/views.py
@@ -3,9 +3,16 @@ from django.shortcuts import render #Used to render templates
 from django.views import generic 
 from .models import Post #Imports all of the blog posts
 from django.db.models import Q #To make queries
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin, #@Login.Required equivalent for class based views
+    UserPassesTestMixin #Checks to see if current user is the correct user
+)
+
 from django.views.generic import (
     DetailView, #Used when making a more detailed page for each blog post
-    CreateView #Used when making a form for users to create blog posts
+    CreateView, #Used when making a form for users to create blog posts
+    UpdateView, #Used so that a user can update a post they have posted
+    DeleteView  #Used so that a user can delete a post they have posted
 )
 
 
@@ -25,15 +32,36 @@ def search(request): #Handles how searches work on my website
 class viewPost(DetailView): #class which defines how individual posts are displayed
     model = Post
 
-class createPost(CreateView): #Class which defines how posts are created
+class createPost(LoginRequiredMixin, CreateView): #Class which defines how posts are created
     model = Post
     fields = ['title', 'slug','content','summary','status','tag1','tag2'] #Fields that can be edited by user
-    def form_valid(self, form):
-        form.instance.userID = self.request.user
-        return super().form_valid(form)
+    def form_valid(self, form): 
+        form.instance.userID = self.request.user #Defines the userID for the post as the ID of the current user
+        return super().form_valid(form) #Returns this value to the form
 
+class editPost(LoginRequiredMixin, UserPassesTestMixin, UpdateView): #Class which defines how posts are edited
+    model = Post
+    fields = ['title', 'slug','content','summary','status','tag1','tag2'] #Fields that can be edited by user
+    def form_valid(self, form): 
+        form.instance.userID = self.request.user #Defines the userID for the post as the ID of the current user
+        return super().form_valid(form) #Returns this value to the form
+    def test_func(self): #Used to check if the current user is the author of the post
+        currentPost = self.get_object() #Assigns the current post to "currentPost"
+        if self.request.user == currentPost.userID: #Checks author of current post to logged in user, if the same...
+            return True #User is let in to the page
+        else: #Otherwise...
+            return False #They are rejected from the page.
 
-
+class deletePost(LoginRequiredMixin, UserPassesTestMixin, DeleteView): #Defines how posts can be deleted
+    model = Post
+    success_url='/'
+    def test_func(self): #Used to check if the current user is the author of the post
+        currentPost = self.get_object() #Assigns the current post to "currentPost"
+        if self.request.user == currentPost.userID: #Checks author of current post to logged in user, if the same...
+            return True #User is let in to the page
+        else: #Otherwise...
+            return False #They are rejected from the page.
+    
 
 
 
